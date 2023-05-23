a0c07d4da5139de32f2f6b33205d26e12bf56b94
maaront <35611834+maaront@users.noreply.github.com>
2023-05-23 15:11:09 -0400

    Set up post model

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Post model (table) by extending off Sequelize's Model class
+class Post extends Model {}
+
+// Set up fields and rules for Post model
+Post.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+  },
+    post_body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    author_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'author',
+            key: 'id',
+        },
+    },  
+},
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
